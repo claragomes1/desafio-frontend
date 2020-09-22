1033027e52f90320b6c1a8279503630e22382a96
Clara Gomes <claraloris39@gmail.com>
2020-09-22 04:10:22 -0300

    teste url

--- src/pages/Pesquisa.js
+++ src/pages/Pesquisa.js
@@ -22,7 +22,7 @@ export default class Pesquisa extends Component {
 
 
     subscribeToEvents = () => {
-        const io = socket('http://localhost:3333');
+        const io = socket(process.env.REACT_APP_API_URL);
         io.on('search', data => {
             this.setState({ info_palavras: [data] })
         })
