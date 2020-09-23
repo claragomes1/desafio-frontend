e0e2c0d7e817555b87e3c828ef958110956afcb4
Clara Gomes <claraloris39@gmail.com>
2020-09-23 19:47:06 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Pesquisa from './pages/Pesquisa';
-import 'materialize-css/dist/css/materialize.min.css';
+//import 'materialize-css/dist/css/materialize.min.css';
 
 
 
